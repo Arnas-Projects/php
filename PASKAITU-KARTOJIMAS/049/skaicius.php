26ef9e81d4f885564c74f797a0a4ac933fc54aca
Arnas-Projects <coding.myprojects@gmail.com>
2026-02-04 10:32:04 +0200

    049 pask kartojimas. Php pradzia. echo

---
+++ PASKAITU-KARTOJIMAS/049/skaicius.php
@@ -0,0 +1,19 @@
+
+<?php
+
+echo '<body style="background-color: #444; color: white; font-size: 24px; line-height: 1.5;">';
+
+
+echo 'Labas, Bebrai';
+
+echo '<br>';
+
+echo 4 + 5;
+
+echo '<br>';
+
+$stringas = 'Kintamasis';
+
+echo $stringas;
+
+
