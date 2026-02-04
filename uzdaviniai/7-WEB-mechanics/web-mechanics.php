c412280ce7050f1675f1067b2c4b9cc4701e6563
Arnas-Projects <coding.myprojects@gmail.com>
2026-02-04 11:03:14 +0200

    #7 nd - web mechanics. 1 uzdavinys.

---
+++ uzdaviniai/7-WEB-mechanics/web-mechanics.php
@@ -0,0 +1,41 @@
+<?php
+// 1. Logic: Decide the color before the HTML starts
+$backgroundColor = 'black'; // Default
+
+if (isset($_GET['color']) && $_GET['color'] == '1') {
+    $backgroundColor = 'red';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Web Mechanics #7</title>
+    <style>
+        body {
+            /* 2. Injection: PHP decides the value here */
+            background-color: <?php echo $backgroundColor; ?>;
+            color: white;
+            padding: 50px;
+        }
+
+        a {
+            color: white;
+            margin-right: 20px;
+            font-size: 20px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <a href="web-mechanics.php">Black Background</a>
+    <a href="web-mechanics.php?color=1">Red Background</a>
+
+    <p>Current color is: <?php echo $backgroundColor; ?></p>
+
+</body>
+
+</html>
