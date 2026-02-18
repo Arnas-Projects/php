e48768195d56e6ff91fb61931280363a813e9ccc
Arnas-Projects <coding.myprojects@gmail.com>
2026-02-18 14:56:12 +0200

    065 paskaita. Astro2 su php ir SQL. Notes kurimas.

--- Astro2/src/App.php
+++ Astro2/src/App.php
@@ -6,28 +6,65 @@ use Astro\Note\Controllers\NoteController;
 
 class App
 {
-    const DIR = __DIR__ . '/../'; // rodo pagrindinį root folderį
+    const DIR = __DIR__ . '/'; // rodo pagrindinį root folderį
     const URL = 'http://astro-project.go/';
     const INSTALL_DIR = '/';
+    const DB_NAME = 'astro2';
 
     public static function run()
     {
-        echo self::router();
+        return self::router();
     }
 
     public static function router()
     {
+        // black box in
+        // stringas iš url http://astro-project.go/ggg/444
         $uri = $_SERVER['REQUEST_URI'];
         // ištrinam tik pradžią
         $uri = preg_replace('#^' . preg_quote(self::INSTALL_DIR) . '#', '', $uri);
         $uri = explode('/', $uri);
+
+        // black box out
+
+        // uri masyvas su parametrais
+        // ['ggg, '444']
+
         $method = $_SERVER['REQUEST_METHOD'];
 
         // echo '<pre>';
         // print_r($uri);
 
         if ('GET' == $method && count($uri) == 1 && $uri[0] == '') {
-            return (new NoteController())->home();
+            $noteController = new NoteController();
+            return $noteController->home();
+
+            // return (new NoteController())->home();
         }
+
+        if ('GET' == $method && count($uri) == 2 && $uri[0] == 'note') {
+            if ('GET' == $method && count($uri) == 2 && $uri[0] == 'note') {
+                return (new NoteController())->show($uri[1]);
+            }
+            // return (new NoteController())->home();
+        }
+
+        if ('GET' == $method && count($uri) == 1 && $uri[0] == 'create') {
+                return (new NoteController())->create();
+            }
+        
+    }
+
+    public static function view(string $template, array $data = [])
+    {
+        extract($data); // indeksai iš masyvo yra paverčiami atskirais kintamaisiais
+
+        // start output buffering
+        ob_start();
+        require self::DIR . 'view/top.php';
+        require self::DIR . "view/{$template}.php";
+        require self::DIR . 'view/bottom.php';
+        // clear output buffer and return result
+        return ob_get_clean();
     }
 }
